06e45ebb17a6a7a521647b94a63ab5fa64abafe8
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-03-30 16:13:14 +0530

    sample lab

--- src/com/ibm/demo/labs/lab1/ex1/Lab1Ex1Demo.java
+++ src/com/ibm/demo/labs/lab1/ex1/Lab1Ex1Demo.java
@@ -1,5 +1,20 @@
 package com.ibm.demo.labs.lab1.ex1;
 
+//Exercise 1: Create a method to 
+// find the sum of the cubes of 
+// the digits of an n-digit number.
+
 public class Lab1Ex1Demo {
+
+	static int sumOfDigits(int num) {
+		// code
+		return 0;
+	}
+
+	public static void main(String[] args) {
+
+		int output = Lab1Ex1Demo.sumOfDigits(123);
+		System.out.println(output);
+	}
 
 }

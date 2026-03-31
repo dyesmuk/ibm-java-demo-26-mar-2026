a6bb59eb283cb55c9979464f196bad7719545f0b
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-03-31 12:58:16 +0530

    day 2

--- src/com/ibm/demo/day2/encap/Employee.java
+++ src/com/ibm/demo/day2/encap/Employee.java
@@ -23,6 +23,29 @@ public class Employee {
 		this.salary = salary;
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
 
 	@Override
 	public String toString() {

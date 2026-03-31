457cace82b3a1f1cce232dd8162114b5f1248343
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-03-31 09:18:43 +0530

    day 2

--- src/com/ibm/demo/labs/lab1/ex1/Lab1Ex1Demo.java
+++ src/com/ibm/demo/labs/lab1/ex1/Lab1Ex1Demo.java
@@ -6,14 +6,21 @@ package com.ibm.demo.labs.lab1.ex1;
 
 public class Lab1Ex1Demo {
 
-	static int sumOfDigits(int num) {
-		// code
-		return 0;
+	public static int sumOfCubesOfDigits(int n) {
+		n = Math.abs(n);
+		int sum = 0;
+		while (n != 0) {
+			int digit = n % 10;
+			sum += digit * digit * digit;
+			n /= 10;
+		}
+
+		return sum;
 	}
 
 	public static void main(String[] args) {
 
-		int output = Lab1Ex1Demo.sumOfDigits(123);
+		int output = Lab1Ex1Demo.sumOfCubesOfDigits(123);
 		System.out.println(output);
 	}
 

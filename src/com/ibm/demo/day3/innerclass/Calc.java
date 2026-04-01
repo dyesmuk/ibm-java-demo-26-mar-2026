a184c8c2dc13adf4f05f083c69e9b6d173a0c19f
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-04-01 12:22:42 +0530

    day 3

---
+++ src/com/ibm/demo/day3/innerclass/Calc.java
@@ -0,0 +1,22 @@
+package com.ibm.demo.day3.innerclass;
+
+public interface Calc {
+
+	public abstract int addNums(int i, int j);
+
+//	public abstract int subtractNums(int i, int j);
+}
+
+//class CalcDemo implements Calc {
+//
+//	@Override
+//	public int addNums(int i, int j) {
+//		return i + j;
+//	}
+//
+//	@Override
+//	public int subtractNums(int i, int j) {
+//		return i - j;
+//	}
+//
+//}

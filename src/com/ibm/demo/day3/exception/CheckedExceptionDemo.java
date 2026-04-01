ff5ce24df965a2ff62dfcedaf899a80389127d48
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-04-01 10:52:39 +0530

    day 3

---
+++ src/com/ibm/demo/day3/exception/CheckedExceptionDemo.java
@@ -0,0 +1,12 @@
+package com.ibm.demo.day3.exception;
+
+public class CheckedExceptionDemo {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
+
+}
--- src/com/ibm/demo/day3/exception/ExceptionDemo.java
+++ src/com/ibm/demo/day3/exception/ExceptionDemo.java
@@ -3,3 +3,5 @@ package com.ibm.demo.day3.exception;
 public class ExceptionDemo {
 
 }
+
+

580662c091c6c2cd3dd9e6195920cd9f8928c3ef
Vaman Deshmukh <vamandeshmukh@gmail.com>
2026-04-02 10:08:32 +0530

    streams api

--- src/com/ibm/demo/day4/streamsapi/Employee.java
+++ src/com/ibm/demo/day4/streamsapi/Employee.java
@@ -17,6 +17,30 @@ public class Employee {
 		this.salary = salary;
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
--- src/com/ibm/demo/day4/streamsapi/StreamsApiDemo.java
+++ src/com/ibm/demo/day4/streamsapi/StreamsApiDemo.java
@@ -3,27 +3,41 @@ package com.ibm.demo.day4.streamsapi;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class StreamsApiDemo {
 
 	public static void main(String[] args) {
 
 		List<Employee> employees = new ArrayList<Employee>(Arrays.asList(new Employee(1, "Monu", 90000),
-				new Employee(2, "Sonu", 95000), new Employee(3, "Tonu", 92000)));
+				new Employee(2, "Sonu", 95000), new Employee(3, "Tonu", 92000), new Employee(4, "Ponu", 97000)));
 
 		System.out.println("Number of employees: " + employees.size());
-		
-		System.out.println("1. List of employees using for loop: ");
 
-		for (int i = 0; i)
-			System.out.println();
+		System.out.println("1. List of employees using for loop: ");
+		for (int i = 0; i < employees.size(); i++)
+			System.out.println(employees.get(i));
 
 		System.out.println("2. List of employees using for each loop: ");
-		
+		for (Employee emp : employees)
+			System.out.println(emp);
 
 		System.out.println("3. List of employees using forEach method: ");
+//		employees.forEach((emp) -> {System.out.println(emp);});
+		employees.forEach(emp -> System.out.println(emp));
+
+		System.out.println("4. List of employees with salary > 91000: ");
+		employees.forEach((emp) -> {
+			if (emp.salary > 91000)
+				System.out.println(emp);
+		});
+
+		System.out.println("5. Increase employees' salary by 10%: ");
+
+		List<Employee> updatedEmployees = employees.stream()
+				.map(e -> new Employee(e.getId(), e.getName(), e.getSalary() * 1.10)).toList();
+		updatedEmployees.forEach(e -> System.out.println(e.toString()));
 
 
 	}
-
 }
